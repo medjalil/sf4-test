602da4b55dba136e6ea8d3933427131ba9f6cf1e
Mohamed Abdeljalil <med.abdeljalil@gmail.com>
2019-12-30 11:06:14 +0100

    rename sqdsd

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
- * @ORM\Table(name="`product`")
+ * @ORM\Table(name="`products`")
  */
 class Product
 {
